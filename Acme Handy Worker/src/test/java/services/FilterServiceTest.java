ebb7d7887fc311dfd0f0bd300bc19b4cdc9200d6
ANTONIO/Anto000 <antonio.alandalus97@gmail.com>
2018-11-29 20:33:27 +0100

    Más Cambios

---
+++ Acme Handy Worker/src/test/java/services/FilterServiceTest.java
@@ -0,0 +1,69 @@
+
+package services;
+
+import java.sql.Date;
+import java.util.Collection;
+
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import domain.Category;
+import domain.Filter;
+import domain.Warranty;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+	"classpath:spring/datasource.xml", "classpath:spring/config/packages.xml"
+})
+@Transactional
+public class FilterServiceTest {
+
+	@Autowired
+	private FilterService	filterService;
+	@Autowired
+	private WarrantyService	warrantyService;
+	@Autowired
+	private CategoryService	categoryService;
+
+
+	@Test
+	public void createFilter() {
+		Filter f;
+
+		final Warranty warranty, savedWarranty;
+		warranty = this.warrantyService.create();
+		final Collection<String> laws = warranty.getLaws();
+		laws.add("Ley1");
+		final Collection<String> terms = warranty.getTerms();
+		terms.add("Term1");
+		warranty.setLaws(laws);
+		warranty.setTerms(terms);
+		warranty.setTitle("TituloWarranty");
+		savedWarranty = this.warrantyService.save(warranty);
+
+		Category category, savedCategory;
+		category = this.categoryService.create();
+		category.setName("CategoriaPrimera");
+		category.setParent(category);
+		savedCategory = this.categoryService.save(category);
+
+		f = this.filterService.create();
+		f.setAddress("Direccion");
+		f.setDescription("Description");
+		f.setEndDate(Date.valueOf("2018-12-12"));
+		f.setHighPrice(12.);
+		f.setLowPrice(10.);
+		f.setStartDate(Date.valueOf("2017-12-12"));
+		f.setWarranty(savedWarranty);
+		f.setCategory(savedCategory);
+
+		Assert.isTrue(f.getDescription().equals("Description"));
+
+	}
+}
